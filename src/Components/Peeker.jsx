85a64146918c2c8071312e76a778cc3ad30aaf19
Patrick Grady <patrickrgrady81@gmail.com>
2021-07-31 11:02:37 -0400

    basic setup

---
+++ src/Components/Peeker.jsx
@@ -0,0 +1,16 @@
+import '../styles/Peeker.scss';
+
+function Peeker() {
+  return (
+    <div className="wrapper">
+      <div className="odds"></div>
+      <div className="table"></div>
+      <div className="pay"></div>
+      <div className="credits"></div>
+      <div className="outcome"></div>
+      <div className="hints"></div>
+    </div>
+  );
+}
+
+export default Peeker;
