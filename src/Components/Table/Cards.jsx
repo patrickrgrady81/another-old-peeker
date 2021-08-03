9cbf7ef5a70a57dc73ad980ca077dfb878bcdfe8
Patrick Grady <patrickrgrady81@gmail.com>
2021-08-03 10:45:39 -0400

    cleanup

--- src/Components/Table/Cards.jsx
+++ src/Components/Table/Cards.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
 import '../../styles/Cards.scss';
 
+import { useSelector } from 'react-redux';
+
 function Cards() {
-    return (
-        <div className="cards">
-            <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
-            <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
-            <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
-            <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
-            <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
-        </div>
-    )
+
+    const hand = useSelector(state => state.hand);
+
+    if (hand.cards) {
+        let cardList = hand.cards.map(card => {
+            return <img key={card.id} src={`/assets/cards/${card.value}${card.suit}.png`} alt="card" className="playing-cards"></img>
+        })
+        
+        return (
+            <div className="cards">
+                <>{cardList}</>
+                
+            </div>
+        )} else {
+            return <h1>Loading...</h1>
+    }
 }
 
-export default Cards
+export default Cards;
+
+// 
+
+
+        // <div className="cards">
+        //     <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
+        //     <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
+        //     <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
+        //     <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
+        //     <img src="/assets/cards/ad.png" alt="card1" className="playing-cards" />
+        // </div>
+
+
+                    // { hand.forEach((card) => {
+            //     return <img src={`/assets/cards/${card.value}${card.suit}.png`} alt="" />
+            // })}
