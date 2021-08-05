64aa9cdcfffe14c6af59693af1c48a171dd0acba
Patrick Grady <patrickrgrady81@gmail.com>
2021-08-05 09:44:44 -0400

    added paytable.js

---
+++ src/Components/paytable.js
@@ -0,0 +1,74 @@
+
+// 610 323 2090 Detective
+const paytable = {
+    "0": [
+        "Credits",
+        "Royal Flush",
+        "Straight Flush",
+        "Four of a Kind",
+        "Full House",
+        "Flush",
+        "Straight",
+        "Three of a Kind",
+        "Two Pair",
+        "Jacks or Better"
+    ], 
+    "1": [
+        250,
+        50,
+        25,
+        9,
+        6,
+        4,
+        3,
+        2,
+        1
+    ],
+    "2": [
+        500,
+        100,
+        50,
+        18,
+        12,
+        8,
+        6,
+        4,
+        2
+    ],
+    "3": [
+        750,
+        150,
+        75,
+        27,
+        18,
+        12,
+        9,
+        6,
+        3
+    ],
+    "4": [
+        1000,
+        200,
+        100,
+        36,
+        24,
+        16,
+        12,
+        8,
+        4
+    ],
+    "5": [
+        4000,
+        250,
+        125,
+        45,
+        30,
+        20,
+        15,
+        10,
+        5
+    ]
+
+}
+
+export default paytable;
